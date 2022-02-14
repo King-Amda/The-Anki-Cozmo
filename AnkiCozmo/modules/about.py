f5279544be3474acd20c6ca6c5e00386911a80db
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-02-14 14:57:13 +0530

    Add files via upload

---
+++ AnkiCozmo/modules/about.py
@@ -0,0 +1,18 @@
+__help__ = """
+** Anki Cozmo - A Powerful Telegram Group Manager And Anki Vector's Brother 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• Group Management With Ban Kick Mute ❗️
+
+• Youtube Downloader 🎵
+
+Developers
+[King Amda🇱🇰](https://t.me/NiupunDinujaya)
+[Damantha Jasinghe🇱🇰](https://t.me/MrItzme)
+[Sadew Jayasekara🇱🇰](https://t.me/Sadew_xD)
+[Black Panther🇱🇰](https://t.me/Im_Black_Panther)
+"""
+__mod_name__ = "About"
